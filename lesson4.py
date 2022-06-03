9e96a7c3c03d5253c454567406a8bd4306a6c286
artemlepekhin <106800455+artemlepekhin@users.noreply.github.com>
2022-06-03 14:46:19 +0300

    test

--- lesson4.py
+++ lesson4.py
@@ -9,7 +9,6 @@ def findCoin(x, y):
     print()
 
 print('Добро пожаловать!')
-print('Где же монетка?')
 while True:
     point_x = float(input('Введите координату X: '))
     point_y = float(input('Введите координату Y: '))
