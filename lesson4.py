4a1dce977dd4df4e0026f4cf6600a7f0f2217150
artemlepekhin <106800455+artemlepekhin@users.noreply.github.com>
2022-06-03 13:27:43 +0300

    initial commit

---
+++ lesson4.py
@@ -0,0 +1,15 @@
+def findCoin(x, y):
+    if -1 <= x <= 1:
+        if -1 <= y <= 1:
+            print('Монетка где-то рядом')
+        else:
+            print('Монетки в области поиска нет')
+    else:
+        print('Монетки в области поиска нет')
+    print()
+
+print('Добро пожаловать!')
+while True:
+    point_x = float(input('Введите координату X: '))
+    point_y = float(input('Введите координату Y: '))
+    findCoin(point_x, point_y)
